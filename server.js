f03416baae5800e7664df1fee4b29f4533b9a4cc
James Funk <james.funk@yardi.com>
2017-06-13 15:41:23 -0700

    Allow dynamic port

--- server.js
+++ server.js
@@ -11,6 +11,6 @@ app.get('/', function (req, res) {
 	res.sendFile(path.join(__dirname + '/templates/index.html'));
 });
 
-app.listen(3000, function () {
+app.listen(process.env.PORT || 3000, function () {
 	console.log('Example app listening on port 3000!')
 });
