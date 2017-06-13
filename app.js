d2922f3c8a4a5f47397a12c3483f82975cd4ff76
James Funk <james.funk@yardi.com>
2017-06-13 15:24:02 -0700

    first commit

---
+++ app.js
@@ -0,0 +1,16 @@
+const express = require('express');
+const app = express();
+const path = require('path');
+
+// app.use(epxress.static('bower_components'));
+app.use('/public', express.static('public'));
+app.use('/bc', express.static('bower_components'));
+app.use('/templates', express.static('templates'));
+
+app.get('/', function (req, res) {
+	res.sendFile(path.join(__dirname + '/templates/index.html'));
+});
+
+app.listen(3000, function () {
+	console.log('Example app listening on port 3000!')
+});
