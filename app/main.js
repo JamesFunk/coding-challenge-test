277df0a4b14cb28fa7aa64f1731d73ceaf7a908e
James Funk <james.funk@yardi.com>
2017-06-14 11:56:44 -0700

    Testing angular fb integration

--- app/main.js
+++ app/main.js
@@ -3,7 +3,8 @@
 
 	const app = angular.module('app', [
 		'ngRoute',
-		'jqueryModule'
+		'jqueryModule',
+		'bluebirdModule'
 	]);
 
 	const contentProviderService = function () {
@@ -20,7 +21,7 @@
 		$routeProvider.when('/', {
 			templateUrl: ContentProvider.getTemplateUrl('main.html')
 		}).when('/login', {
-			template: '<div>Time to log in</div>'
+			template: '<div><fb:login-button show-faces="true" max-rows="1" size="large"></fb:login-button></div>'
 		}).otherwise('/');
 	}]);
 
@@ -34,7 +35,48 @@
 			return $window.jQuery;
 		}]);
 
+	angular.module('bluebirdModule', [])
+		.factory('P', ['$window', function ($window) {
+			return $window.P;
+		}]);
+
 	app.factory('ContentProvider', contentProviderService);
+
+	app.run(['$rootScope', '$window', 'srvAuth', function ($rootScope, $window, sAuth) {
+		$rootScope.user = {};
+
+		$window.fbAsyncInit = function() {
+			FB.init({
+				appId: '1563656153652814',
+				status: true,
+				channelUrl: 'templates/channel.html',
+				autoLogAppEvents : true,
+				xfbml            : true,
+				version          : 'v2.9'
+			});
+			sAuth.watchLoginChange();
+		};
+
+		(function(d){
+			// load the Facebook javascript SDK
+
+			var js,
+				id = 'facebook-jssdk',
+				ref = d.getElementsByTagName('script')[0];
+
+			if (d.getElementById(id)) {
+				return;
+			}
+
+			js = d.createElement('script');
+			js.id = id;
+			js.async = true;
+			js.src = "//connect.facebook.net/en_US/all.js";
+
+			ref.parentNode.insertBefore(js, ref);
+
+		}(document));
+	}]);
 
 	app.controller('appCtrl', ['$scope', '$location', function ($scope, $location) {
 		$scope.variable = 'Haaaallo world!';
@@ -45,5 +87,54 @@
 		$scope.add = function() {
 			$scope.stuff = $scope.stuff + 1;
 		}
+	}]);
+
+	app.service('srvAuth', [function () {
+		const watchLoginChange = function() {
+			const _self = this;
+			FB.Event.subscribe('auth.authResponseChange', function(res) {
+				if (res.status === 'connected') {
+					/*
+					 The user is already logged,
+					 is possible retrieve his personal info
+					 */
+					_self.getUserInfo();
+					/*
+					 This is also the point where you should create a
+					 session for the current user.
+					 For this purpose you can use the data inside the
+					 res.authResponse object.
+					 */
+				}
+				else {
+					/*
+					 The user is not logged to the app, or into Facebook:
+					 destroy the session on the server.
+					 */
+				}
+			});
+		};
+
+		const getUserInfo = function() {
+			const _self = this;
+			FB.api('/me', function(res) {
+				$rootScope.$apply(function() {
+					$rootScope.user = _self.user = res;
+				});
+			});
+		};
+		const logout = function() {
+			const _self = this;
+			FB.logout(function(response) {
+				$rootScope.$apply(function() {
+					$rootScope.user = _self.user = {};
+				});
+			});
+		};
+		return {
+			watchLoginChange: watchLoginChange,
+			getUserInfo: getUserInfo,
+			logout: logout
+		};
 	}]);
 })();
